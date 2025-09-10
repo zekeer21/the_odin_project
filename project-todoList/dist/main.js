b6fbc97f56742df2b64283e6ebf2c84887804e76
zekeer21 <achas241@gmail.com>
2025-09-10 19:43:01 +0800

    added styles and gitignore

--- project-todoList/dist/main.js
+++ project-todoList/dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-
+throw new Error("Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n> import \"./style.css\"");
 
 /***/ })
 
@@ -24,7 +24,7 @@
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
